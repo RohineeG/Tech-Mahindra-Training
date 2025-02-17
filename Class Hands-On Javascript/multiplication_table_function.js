e87d4d08492f41ce631d7c16717cd6014460887d
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-02-17 09:44:15 +0530

    Create multiplication_table_function.js

---
+++ Class Hands-On Javascript/multiplication_table_function.js
@@ -0,0 +1,11 @@
+function multiplicationTable() {
+    let number = 15;  
+    let limit = 8;  
+    console.log(`Multiplication Table of ${number} up to ${limit}:`);
+    let i = 1;
+    while (i <= limit) {
+        console.log(`${number} x ${i} = ${number * i}`);
+        i++;
+    }
+}
+MultiplicationTable();
