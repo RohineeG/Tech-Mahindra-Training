aa64f22fc9999332e0252b8676e00a65e6f1e85e
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-02-17 09:58:01 +0530

    Create sumOfDigits.js

---
+++ Class Hands-On Javascript/sumOfDigits.js
@@ -0,0 +1,17 @@
+function sumOfDigits(num){
+    let sum = 0;
+    let temp = num;
+    while(temp>0){
+        sum += temp%10;
+        temp = Math.floor(temp/10);
+    }
+    console.log(sum);
+    if(sum%2==0){
+        console.log("Sum is even");
+    }
+    else{
+        console.log("Sum is odd");
+    }
+}
+let number = prompt("Enter the number: ");
+sumOfDigits(number);
