921e826e9b8fbb1eb48736b006fbe0dbedeaddbd
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-02-22 23:42:58 +0530

    Update ByteArray.java

--- Core Java Tasks/Task 3/ByteArray.java
+++ Core Java Tasks/Task 3/ByteArray.java
@@ -7,8 +7,8 @@ public class ByteArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the file path: ");
-        String filePath = sc.nextLine();
-        File file = new File(filePath);
+        String path = sc.nextLine();
+        File file = new File(path);
         if (file.exists() && file.isFile()) {
             try {
                 FileInputStream fis = new FileInputStream(file);
