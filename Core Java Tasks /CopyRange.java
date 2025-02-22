5fb509bab0c6f03fe2d8c98174d329a2a0984ccd
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-02-22 22:50:07 +0530

    Delete Core Java Tasks  directory

--- Core Java Tasks /CopyRange.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-public class CopyRange {
-    public static void main(String[] args) {
-        int[] array1 = {10, 20, 30, 40,};
-        int[] array2 = Arrays.copyOfRange(array1, 1, 3);
-        System.out.println("Copied Array: " + Arrays.toString(array2));
-    }
-}
--- Core Java Tasks /Equals.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Equals {
-    public static void main(String[] args) {
-        Integer[][] array1 = {{1, 2}, {3, 4}};
-        Integer[][] array2 = {{1, 2}, {3, 4}};
-        System.out.println("Using equals(): " + Arrays.equals(array1, array2));     
-        System.out.println("Using deepEquals(): " + Arrays.deepEquals(array1, array2)); 
-    }
-}
