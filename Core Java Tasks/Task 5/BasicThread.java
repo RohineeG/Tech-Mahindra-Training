62a5e9580d98ef76d01b5dd0c3d3525252d8a84f
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-02-24 07:45:12 +0530

    Create BasicThread.java

---
+++ Core Java Tasks/Task 5/BasicThread.java
@@ -0,0 +1,10 @@
+class BasicThread extends Thread {
+    public void run() {
+        System.out.println("Hello, Java!");
+    }
+
+    public static void main(String[] args) {
+        BasicThread thread = new BasicThread();
+        thread.start();
+    }
+}
