543e8fa367523c2fb92cd2b2ede26434f34bfe23
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-02-24 13:34:34 +0530

    Update SortStrings.java

--- Core Java Tasks/Task 9/SortStrings.java
+++ Core Java Tasks/Task 9/SortStrings.java
@@ -4,15 +4,8 @@ import java.util.stream.Collectors;
 public class SortStrings {
     public static void main(String[] args) {
         List<String> words = List.of("elephant", "lion", "tiger", "goat");
-
-        List<String> ascendingOrder = words.stream()
-                .sorted()
-                .collect(Collectors.toList());
-
-        List<String> descendingOrder = words.stream()
-                .sorted((a, b) -> b.compareTo(a))
-                .collect(Collectors.toList());
-
+        List<String> ascendingOrder = words.stream().sorted().collect(Collectors.toList());
+        List<String> descendingOrder = words.stream().sorted((a, b) -> b.compareTo(a)).collect(Collectors.toList());
         System.out.println("Ascending Order: " + ascendingOrder);
         System.out.println("Descending Order: " + descendingOrder);
     }
