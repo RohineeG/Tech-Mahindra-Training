8900045d460b78ee594865a13a127feeb4cac0fc
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-02-24 13:33:32 +0530

    Update CountStrings.java

--- Core Java Tasks/Task 9/CountStrings.java
+++ Core Java Tasks/Task 9/CountStrings.java
@@ -4,12 +4,8 @@ import java.util.List;
 public class CountStrings {
     public static void main(String[] args) {
         List<String> words = List.of("elephant", "peacock", "tiger", "dog");
-
         char targetLetter = 'p';
-
-        long count = words.stream()
-                .filter(word -> word.startsWith(String.valueOf(targetLetter)))
-                .count();
+        long count = words.stream().filter(word -> word.startsWith(String.valueOf(targetLetter))).count();
 
         System.out.println("Number of words starting with '" + targetLetter + "': " + count);
     }
