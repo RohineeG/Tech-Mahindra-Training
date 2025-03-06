986704f13d1dda536ce571e6e16a6e8fbef72156
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-03-06 10:48:01 +0530

    Add files via upload

---
+++ Angular Hands-On/src/app/model/project.ts
@@ -0,0 +1,10 @@
+export interface projects{
+    id:string;
+    name:string;
+    description:string;
+    status:string;
+    startDate:string;
+    endDate:string;
+    teamSize:string;
+    image:string
+}
