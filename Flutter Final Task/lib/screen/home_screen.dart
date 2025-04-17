0c5922cce516b4e9b138042cfd32798a1598be5a
Rohinee <145481795+RohineeG@users.noreply.github.com>
2025-04-17 14:25:52 +0530

    Update home_screen.dart

--- Flutter Final Task/lib/screen/home_screen.dart
+++ Flutter Final Task/lib/screen/home_screen.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'categories.dart'; // Replace with correct path
+import 'favorites_screen.dart'; // Replace with correct path
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Test Home')),
+      appBar: AppBar(title: Text('Explorer')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () => print("Button Pressed!"),
-          child: Text("Press Me"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome!!!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Explore top tourist spots of Chennai',
+              style: TextStyle(fontSize: 16, color: Colors.Blue),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 32), // Space between text and buttons
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Categories()),
+                );
+              },
+              child: Text("Explore Destinations"),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FavoritesScreen()),
+                );
+              },
+              child: Text("Favorites"),
+            ),
+          ],
         ),
       ),
     );
